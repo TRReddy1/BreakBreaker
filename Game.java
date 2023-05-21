2f1d07456873e79bf5daeace72f1fadbd8d5ff31
TRReddy1 <92451400+TRReddy1@users.noreply.github.com>
2023-05-21 15:40:19 +0530

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Game {
+
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+}
